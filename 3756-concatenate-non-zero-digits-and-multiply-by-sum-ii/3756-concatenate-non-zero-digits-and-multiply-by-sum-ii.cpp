97ea073787d695a13071571fe7aee5518ba45f69
Arindam Jaiman <arindamjaiman@gmail.com>
2026-07-08 13:24:58 +0530

    Time: 51 ms (42.49%), Space: 162.2 MB (38.86%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    static constexpr long long MOD = 1000000007;
+    static constexpr int MOD = 1000000007;
+
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.size();
+
         vector<int> pos;
         vector<int> digits;
-        for (int i = 0; i < s.size(); i++) {
+        pos.reserve(n);
+        digits.reserve(n);
+        for (int i = 0; i < n; i++) {
             if (s[i] != '0') {
                 pos.push_back(i);
                 digits.push_back(s[i] - '0');
@@ -12,10 +17,39 @@ public:
         }
 
         int k = digits.size();
+        if (k == 0) {
+            return vector<int>(queries.size(), 0);
+        }
+        vector<int> nextIdx(n, k);
+
+        int ptr = k - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            while (ptr >= 0 && pos[ptr] > i)
+                ptr--;
+
+            if (ptr >= 0 && pos[ptr] == i)
+                nextIdx[i] = ptr;
+            else if (ptr + 1 < k)
+                nextIdx[i] = ptr + 1;
+        }
+        vector<int> prevIdx(n, -1);
+
+        ptr = 0;
+        for (int i = 0; i < n; i++) {
+            while (ptr < k && pos[ptr] < i)
+                ptr++;
+
+            if (ptr < k && pos[ptr] == i)
+                prevIdx[i] = ptr;
+            else if (ptr > 0)
+                prevIdx[i] = ptr - 1;
+        }
+
         vector<long long> pow10(k + 1, 1);
         for (int i = 1; i <= k; i++)
             pow10[i] = (pow10[i - 1] * 10) % MOD;
         vector<long long> prefVal(k + 1, 0);
+
         vector<int> prefSum(k + 1, 0);
 
         for (int i = 0; i < k; i++) {
@@ -24,15 +58,16 @@ public:
         }
 
         vector<int> ans;
+        ans.reserve(queries.size());
 
-        for (auto &q : queries) {
+        for (const auto &q : queries) {
             int l = q[0];
             int r = q[1];
 
-            int L = lower_bound(pos.begin(), pos.end(), l) - pos.begin();
-            int R = upper_bound(pos.begin(), pos.end(), r) - pos.begin() - 1;
+            int L = nextIdx[l];
+            int R = prevIdx[r];
 
-            if (L > R) {
+            if (L > R || L == k || R == -1) {
                 ans.push_back(0);
                 continue;
             }
@@ -48,7 +83,6 @@ public:
 
             ans.push_back((x * sum) % MOD);
         }
-
         return ans;
     }
 };
